4b8329009e1372239e384ee9d28997833743dbab
skpixels <makkobir79@gmail.com>
2023-03-19 14:55:22 +0600

    reactangle finished

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Triangle calculation
 document.getElementById("triangle-btn").addEventListener("click", function () {
-  const triangleBInputField = document.getElementById("triangle-input-filed-b");
-  const triangleHInputField = document.getElementById("triangle-input-filed-h");
+  const triangleBInputField = document.getElementById("triangle-input-field-b");
+  const triangleHInputField = document.getElementById("triangle-input-field-h");
   const triangleBValue = triangleBInputField.value;
   const triangleHValue = triangleHInputField.value;
 
@@ -9,4 +9,21 @@ document.getElementById("triangle-btn").addEventListener("click", function () {
 
   // calculate the triangle area
   const result = initialValue * triangleBValue * triangleHValue;
+  console.log(result);
+});
+
+// rectangle calculation
+document.getElementById("rectangle-btn").addEventListener("click", function () {
+  const triangleWidthInputField = document.getElementById(
+    "rectangle-input-field-w"
+  );
+  const triangleLengthInputField = document.getElementById(
+    "rectangle-input-field-l"
+  );
+  const triangleWidthValue = triangleWidthInputField.value;
+  const triangleLengthValue = triangleLengthInputField.value;
+
+  // calculate the triangle area
+  const result = triangleWidthValue * triangleLengthValue;
+  console.log(result);
 });
